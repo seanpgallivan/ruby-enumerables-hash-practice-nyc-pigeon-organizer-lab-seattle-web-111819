834a18ac7e95de779d50c770e419e4d19639b0b8
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-15 01:41:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new = {}
+  data[:color].reduce(new) do |memo, (k, v)|
+    i = 0
+    while i < v.length do
+      memo[v[i]] = {color: [], gender: [], lives: []} if !memo[v[i]]
+      memo[v[i]][:color] << k.to_s
+      i += 1
+    end
+    memo
+  end
+  data[:gender].reduce(new) do |memo, (k, v)|
+    i = 0
+    while i < v.length do
+      memo[v[i]] = {color: [], gender: [], lives: []} if !memo[v[i]]
+      memo[v[i]][:gender] << k.to_s
+      i += 1
+    end
+    memo
+  end
+  data[:lives].reduce(new) do |memo, (k, v)|
+    i = 0
+    while i < v.length do
+      memo[v[i]] = {color: [], gender: [], lives: []} if !memo[v[i]]
+      memo[v[i]][:lives] << k.to_s
+      i += 1
+    end
+    memo
+  end
+  pp new
 end
