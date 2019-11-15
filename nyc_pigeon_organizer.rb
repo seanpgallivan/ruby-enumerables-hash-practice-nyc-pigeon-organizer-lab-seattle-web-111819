8ded28c9c583ec90c86af555b419ccded2824043
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-15 00:44:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+  pigeon_data = {
+    :color => {
+      :purple => ["Theo", "Peter Jr.", "Lucky"],
+      :grey => ["Theo", "Peter Jr.", "Ms. K"],
+      :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+      :brown => ["Queenie", "Alex"]
+    },
+    :gender => {
+      :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+      :female => ["Queenie", "Ms. K"]
+    },
+    :lives => {
+      "Subway" => ["Theo", "Queenie"],
+      "Central Park" => ["Alex", "Ms. K", "Lucky"],
+      "Library" => ["Peter Jr."],
+      "City Hall" => ["Andrew"]
+    }
+  }
+
+
 def nyc_pigeon_organizer(data)
   new = {}
   names = data[:lives].values.flatten.sort
