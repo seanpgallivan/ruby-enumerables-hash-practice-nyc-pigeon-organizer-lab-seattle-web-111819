e202ec38da8db594359ee5684759f589739fa585
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-15 01:41:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,23 +1,3 @@
-pigeon_data = {
-  :color => {
-    :purple => ["Theo", "Peter Jr.", "Lucky"],
-    :grey => ["Theo", "Peter Jr.", "Ms. K"],
-    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-    :brown => ["Queenie", "Alex"]
-  },
-  :gender => {
-    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-    :female => ["Queenie", "Ms. K"]
-  },
-  :lives => {
-    "Subway" => ["Theo", "Queenie"],
-    "Central Park" => ["Alex", "Ms. K", "Lucky"],
-    "Library" => ["Peter Jr."],
-    "City Hall" => ["Andrew"]
-  }
-}
-
-
 def nyc_pigeon_organizer(data)
   new = {}
   data[:color].reduce(new) do |memo, (k, v)|
@@ -49,4 +29,3 @@ def nyc_pigeon_organizer(data)
   end
   pp new
 end
-# nyc_pigeon_organizer(pigeon_data)
